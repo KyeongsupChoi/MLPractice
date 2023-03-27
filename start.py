750f3608f35a9daf06f469cd9276d79754ae1d84
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2023-03-27 16:36:58 +0900

    tree visualization

--- start.py
+++ start.py
@@ -2,6 +2,7 @@ from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn import tree
 
 
 # Load Dataframe from csv
@@ -16,10 +17,17 @@ y = df["charges"]
 # Set features
 X = df[["age", "bmi", "children"]]
 
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+# Test split
+train_X, val_X, train_y, val_y = train_test_split(X, y, test_size=0.3, random_state=0)
 
 # Fit model
 dec_model.fit(train_X, train_y)
 
+tree.export_graphviz(dec_model,out_file='tree.dot')
+
 val_predictions = dec_model.predict(val_X)
 
+print(val_predictions)
+print(list(val_y))
+
+print(df)
